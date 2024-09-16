4e8349c187ffa1e73f4f1e032ff7c106c9ca8b52
sym8979 <sym8979@gmail.com>
2024-09-16 19:17:39 +0900

    add history length limit to 1000

--- web-client/src/service/historyService.js
+++ web-client/src/service/historyService.js
@@ -4,6 +4,9 @@ const get = (bot_id) => {
   return apiService.post('/bot/get_bot_chat_history', bot_id);
 };
 const update = (bot_id, histories) => {
+  while (histories.length > 1000) {
+    histories.shift();
+  }
   return apiService.post('/bot/update_bot_chat_history', {bot_id, histories: JSON.stringify(histories)});
 }
 const historyService = {
